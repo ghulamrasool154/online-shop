47f3734f11c9b2408cca752359ed953d45646dd1
Ghulam Rasool <grsaddiqui75@gmail.com>
2023-06-15 07:40:31 +0500

    working

--- src/client/components/FeatureProducts/FeatureProduct.jsx
+++ src/client/components/FeatureProducts/FeatureProduct.jsx
@@ -1,31 +1,26 @@
 import React from "react";
 import "./feature.css";
+import img1 from "../../../assets/images/color-logo.png";
 const FeatureProduct = () => {
   return (
     <div className="feature">
       <div className="container-xxl">
         <div className="row">
           <div className="col">
-            <div className="d-flex">
+            <div className="d-flex justify-content-between">
               {[1, 2, 3, 4].map(() => {
                 return (
                   <div class="item-product">
-                    <img src="/img/products/f3.jpg" alt="" />
-                    <div class="des">
-                      <span>Adidas</span>
+                    <div className="img-detail">
+                      <img src={img1} alt="" className="img-fluid" />
+                    </div>
+                    <div class="descriptin">
                       <h5>Cartoon Astronaut T-Shirt</h5>
-                      <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                      </div>
                       <h4>$78</h4>
                     </div>
-                    <a href="#">
-                      <i class="fal fa-shopping-cart cart"></i>
-                    </a>
+                    <div>
+                      <a href="/proudct-detail/11">view more</a>
+                    </div>
                   </div>
                 );
               })}
